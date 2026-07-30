ea74e50986f2bf063e8724f6e1c35582346775d9
Piyush Suthar <141106889+Piyush9727@users.noreply.github.com>
2026-07-30 17:33:56 +0530

    Automatic commit from Bubble

---
+++ elements/AAw-850qe/reset.js
@@ -0,0 +1,15 @@
+function(instance, context) {
+  if (!instance.data) return;
+  instance.data.hasInitializedDefaults = false;
+  if (typeof instance.data.clearAll === 'function') {
+    instance.data.clearAll();
+  } else {
+    instance.data.selectedThings = [];
+    if (typeof instance.data.renderChips === 'function') instance.data.renderChips();
+    if (typeof instance.data.close === 'function') instance.data.close();
+    instance.publishState('value', []);
+    instance.publishState('value_count', 0);
+    instance.publishState('search_term', '');
+    if (typeof instance.data.updateValidation === 'function') instance.data.updateValidation();
+  }
+}
